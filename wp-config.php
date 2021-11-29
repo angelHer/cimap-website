d40020b7de969dc99e16549be15daa84f7cdce71
Angel Hernandez <angel_her_li@hotmail.com>
2021-11-29 17:22:26 -0600

    CHANGE: files permissions

--- wp-config.php
+++ wp-config.php
@@ -79,10 +79,10 @@ $table_prefix = 'wp_';
  *
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', false );
+define( 'WP_DEBUG', true );
 
 /* Add any custom values between this line and the "stop editing" line. */
-
+define( 'FS_METHOD', 'direct' );
 
 
 /* That's all, stop editing! Happy publishing. */
