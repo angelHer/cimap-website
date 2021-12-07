60d0e5aaa18e3cc610e5fe2d15713f69fdef03a8
Angel Hernandez <angel_her_li@hotmail.com>
2021-12-07 00:10:17 -0600

    CHANGE: menu links

--- wp-content/themes/cimap/front-page.php
+++ wp-content/themes/cimap/front-page.php
@@ -18,7 +18,7 @@
 </div>
 
 <!-- Nosotros -->
-<div class="about">
+<div class="about" id="about">
     <div class="container py-5">
         <div class="row">
             <div class="col-12 col-md-6">
--- wp-content/themes/cimap/header.php
+++ wp-content/themes/cimap/header.php
@@ -12,23 +12,25 @@
 <body>
 
     <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light shadow">
             <div class="container-fluid">
-                <img class="navbar-brand header__logo" src="<?php echo home_url(); ?>/wp-content/themes/cimap/public/images/logo-cimap-150x150.png" alt ="">
+                <a class="navbar-brand" href="#">
+                    <img class="navbar-brand header__logo" src="<?php echo home_url(); ?>/wp-content/themes/cimap/public/images/logo-cimap-150x150.png" alt ="">
+                </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
+                            <a class="nav-link" href="#about">Nosotros</a>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
+                                Servicios / productos
                             </a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                 <li><a class="dropdown-item" href="#">Action</a></li>
@@ -40,7 +42,13 @@
                             </ul>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link disabled">Disabled</a>
+                            <a class="nav-link" href="#">Blog</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Clientes</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contacto</a>
                         </li>
                     </ul>
                 </div>
