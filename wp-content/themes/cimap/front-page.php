79684ca9cd917a96a8242b26ecc1b0a0554980a0
Angel Hernandez <angel_her_li@hotmail.com>
2021-12-09 23:50:33 -0600

    CHANGE: Services images

--- wp-content/themes/cimap/front-page.php
+++ wp-content/themes/cimap/front-page.php
@@ -42,10 +42,10 @@
 
 <!-- Nosotros -->
 <div class="about" id="about">
-    <div class="container py-5">
+    <div class="container py-5 h-100 d-flex align-items-center">
         <div class="row">
             <div class="col-12 col-md-6">
-                <h1>Nosotros</h1>
+                <h2 class="about__title">Nosotros</h2>
                 <p>Nuestro equipo te ofrece la información necesaria para <span class="about__text--highlighted">estar enterado de lo que tu marca genera en materia de medios de comunicación.</span></p>
                 <p>Somos un ente externo a tu organización que, de manera profesional y objetiva, da <span class="about__text--highlighted">seguimiento a tu información para detectar áreas de oportunidad.</span></p>
                 <p>Trascender tu comunicación y elevar el posicionamiento a través de una estrategia exitosa, es nuestro trabajo.</p>
@@ -58,62 +58,74 @@
 <div>
     <div class="row w-100 m-0">
         <div class="col-6 col-sm-4 col-md-2 col-lg p-0 services__service">
-            <img class="services__image img-fluid" src="<?php echo home_url(); ?>/wp-content/uploads/2021/12/boton-analisis.jpg" alt="" data-bs-custom-class="services__tooltip" data-bs-toggle="tooltip" data-bs-html="true" title="<h2>The tooltip text Extra text</h2><p>contenido del tooltip</p><a href='#'>Mas</a>">
+            <img
+                class="services__image img-fluid"
+                src="<?php echo home_url(); ?>/wp-content/uploads/2021/12/boton-sintesis.jpg"
+                alt=""
+                data-bs-custom-class="services__tooltip"
+                data-bs-toggle="tooltip"
+                data-bs-html="true"
+                title="
+                    <h2 class='services__tooltip-title'>Síntesis digital de noticias</h2>
+                    <p>Documento diario que contiene lo más destacable de la prensa local y nacional en medios impresos y digitales.</p>
+                    <p>Estar informado siempre es un elemento clave en la toma de decisiones.</p>
+                "
+            >
             <div class="px-2">Síntesis digital de noticias</div>
         </div>
         <div class="col-6 col-sm-4 col-md-2 col-lg p-0 services__service">
-            <img class="services__image img-fluid" src="<?php echo home_url(); ?>/wp-content/uploads/2021/12/boton-analisis.jpg" alt="">
-            <div class="px-2">Síntesis digital de noticias</div>
+            <img class="services__image img-fluid" src="<?php echo home_url(); ?>/wp-content/uploads/2021/12/boton-monitoreo.jpg" alt="">
+            <div class="px-2">Monitoreo de medios</div>
+        </div>
+        <div class="col-6 col-sm-4 col-md-2 col-lg p-0 services__service">
+            <img class="services__image img-fluid" src="<?php echo home_url(); ?>/wp-content/uploads/2021/12/boton-foda.jpg" alt="">
+            <div class="px-2">Diagnóstico FODA</div>
         </div>
         <div class="col-6 col-sm-4 col-md-2 col-lg p-0 services__service">
             <img class="services__image img-fluid" src="<?php echo home_url(); ?>/wp-content/uploads/2021/12/boton-analisis.jpg" alt="">
-            <div class="px-2">Síntesis digital de noticias</div>
+            <div class="px-2">Análisis</div>
         </div>
         <div class="col-6 col-sm-4 col-md-2 col-lg p-0 services__service">
-            <img class="services__image img-fluid" src="<?php echo home_url(); ?>/wp-content/uploads/2021/12/boton-analisis.jpg" alt="">
-            <div class="px-2">Síntesis digital de noticias</div>
+            <img class="services__image img-fluid" src="<?php echo home_url(); ?>/wp-content/uploads/2021/12/boton-manejocrisis.jpg" alt="">
+            <div class="px-2">Manejo de crisis</div>
         </div>
         <div class="col-6 col-sm-4 col-md-2 col-lg p-0 services__service">
-            <img class="services__image img-fluid" src="<?php echo home_url(); ?>/wp-content/uploads/2021/12/boton-analisis.jpg" alt="">
-            <div class="px-2">Síntesis digital de noticias</div>
-        </div>
-        <div class="col-6 col-sm-4 col-md-2 col-lg p-0 services__service">
-            <img class="services__image img-fluid" src="<?php echo home_url(); ?>/wp-content/uploads/2021/12/boton-analisis.jpg" alt="">
-            <div class="px-2">Síntesis digital de noticias</div>
+            <img class="services__image img-fluid" src="<?php echo home_url(); ?>/wp-content/uploads/2021/12/boton-estrategiasintegrales.jpg" alt="">
+            <div class="px-2">Estrategias integrales de comunicación</div>
         </div>
         <div class="col-6 col-sm-4 col-md-2 col-lg p-0 services__service">
             <img class="services__image img-fluid" src="<?php echo home_url(); ?>/wp-content/uploads/2021/12/boton-analisis.jpg" alt="" data-bs-custom-class="services__tooltip" data-bs-toggle="tooltip" data-bs-html="true" title="The tooltip text Extra text">
-            <div class="px-2">Síntesis digital de noticias</div>
+            <div class="px-2">Encuestas organizacionales</div>
         </div>
 
 
         <div class="col-6 col-sm-4 col-md-2 col-lg p-0 services__service">
-            <img class="services__image img-fluid" src="<?php echo home_url(); ?>/wp-content/uploads/2021/12/boton-analisis.jpg" alt="">
-            <div class="px-2">Síntesis digital de noticias</div>
+            <img class="services__image img-fluid" src="<?php echo home_url(); ?>/wp-content/uploads/2021/12/boton-capacitaciones.jpg" alt="">
+            <div class="px-2">Capacitaciones integrales</div>
         </div>
         <div class="col-6 col-sm-4 col-md-2 col-lg p-0 services__service">
-            <img class="services__image img-fluid" src="<?php echo home_url(); ?>/wp-content/uploads/2021/12/boton-analisis.jpg" alt="">
-            <div class="px-2">Síntesis digital de noticias</div>
+            <img class="services__image img-fluid" src="<?php echo home_url(); ?>/wp-content/uploads/2021/12/boton-creacioncontenido.jpg" alt="">
+            <div class="px-2">Creación de contenido institucional</div>
         </div>
         <div class="col-6 col-sm-4 col-md-2 col-lg p-0 services__service">
-            <img class="services__image img-fluid" src="<?php echo home_url(); ?>/wp-content/uploads/2021/12/boton-analisis.jpg" alt="">
-            <div class="px-2">Síntesis digital de noticias</div>
+            <img class="services__image img-fluid" src="<?php echo home_url(); ?>/wp-content/uploads/2021/12/boton-asesorias.jpg" alt="">
+            <div class="px-2">Asesorías personalizadas</div>
         </div>
         <div class="col-6 col-sm-4 col-md-2 col-lg p-0 services__service">
-            <img class="services__image img-fluid" src="<?php echo home_url(); ?>/wp-content/uploads/2021/12/boton-analisis.jpg" alt="">
-            <div class="px-2">Síntesis digital de noticias</div>
+            <img class="services__image img-fluid" src="<?php echo home_url(); ?>/wp-content/uploads/2021/12/boton-creacioncampana.jpg" alt="">
+            <div class="px-2">Creación de campañas</div>
         </div>
         <div class="col-6 col-sm-4 col-md-2 col-lg p-0 services__service">
-            <img class="services__image img-fluid" src="<?php echo home_url(); ?>/wp-content/uploads/2021/12/boton-analisis.jpg" alt="">
-            <div class="px-2">Síntesis digital de noticias</div>
+            <img class="services__image img-fluid" src="<?php echo home_url(); ?>/wp-content/uploads/2021/12/boton-mktingdigital.jpg" alt="">
+            <div class="px-2">Marketing digital y manejo de redes sociales</div>
         </div>
         <div class="col-6 col-sm-4 col-md-2 col-lg p-0 services__service">
-            <img class="services__image img-fluid" src="<?php echo home_url(); ?>/wp-content/uploads/2021/12/boton-analisis.jpg" alt="">
-            <div class="px-2">Síntesis digital de noticias</div>
+            <img class="services__image img-fluid" src="<?php echo home_url(); ?>/wp-content/uploads/2021/12/boton-videofotografia.jpg" alt="">
+            <div class="px-2">Video y fotografía institucional</div>
         </div>
         <div class="col-6 col-sm-4 col-md-2 col-lg p-0 services__service">
-            <img class="services__image img-fluid" src="<?php echo home_url(); ?>/wp-content/uploads/2021/12/boton-analisis.jpg" alt="">
-            <div class="px-2">Síntesis digital de noticias</div>
+            <img class="services__image img-fluid" src="<?php echo home_url(); ?>/wp-content/uploads/2021/12/boton-espacios.jpg" alt="">
+            <div class="px-2">Espacios y pautas en nuestra página, síntesis y agencia de noticias</div>
         </div>
     </div>
 </div>
