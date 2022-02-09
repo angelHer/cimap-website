00bf7ab93c5a8037eee8b96ffb33a00870ac5e15
Angel Hernandez <angel_her_li@hotmail.com>
2022-02-08 21:36:23 -0600

    CHANGE: single post column size

--- wp-content/themes/cimap/single-post_type_servicios.php
+++ wp-content/themes/cimap/single-post_type_servicios.php
@@ -7,11 +7,11 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <div class="container">
         <div class="row">
-            <div class="col">
+            <div class="col-12 col-sm-12 col-md-6">
                 <h1 class="single-post__title"><?php the_title(); ?></h1>
                 <?php the_content(); ?>
             </div>
-            <div class="col">
+            <div class="col-12 col-sm-12 col-md-6 d-flex align-items-center">
                 <img src="<?php echo $image_url ?>" class="img-fluid">
             </div>
         </div>
